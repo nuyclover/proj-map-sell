6b4f234c97ef09e5410077b63b6d7cf72056a2e5
Nuyclover <nuyclover08z@gmail.com>
2021-01-25 00:16:57 +0700

    install bootstrap

---
+++ resources/js/components/Elements/TextInputComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="">
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -19,6 +18,8 @@
                 font-family: 'Nunito';
             }
         </style>
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
@@ -128,5 +129,7 @@
                 </div>
             </div>
         </div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
